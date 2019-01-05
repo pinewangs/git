84d1db3d730980e791428df9d758fdc89a39d3e1
pine <pine@qq.com>
2019-01-05 20:48:54 +0800

    ssss

---
+++ src/main/java/com/atguigu/entiti/Eployee.java
@@ -0,0 +1,6 @@
+package com.atguigu.entiti;
+
+public class Eployee {
+
+
+}
---
+++ src/main/java/com/atguigu/entiti/SvnTest.java
@@ -0,0 +1,9 @@
+package com.atguigu.entiti;
+
+public class SvnTest {
+	
+	public static void main(String[] args) {
+		System.err.println("aaaa");
+	}
+
+}
